b215b0bd8aefa5fcaa0523569a47b0fab92355de
RMeuter <roro7@hotmail.fr>
2021-11-02 14:15:41 +0100

    Init branch

---
+++ database_Managing/InformationDatabase.md
@@ -0,0 +1,8 @@
+# Database firebase
+
+Manage spatial query on firebase: https://firebase.google.com/docs/firestore/solutions/geoqueries
+
+## Why firebase ?
+
+L'utilisation de firebase est du au fait que nous maitrisions déjà des compétences sur ce type de base de donnée.
+Nous adaptons l'utilisation de la base de donnée à des requetes spatial.
---
+++ database_Managing/database.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
