05bc17367edcd0f0c1372a18e693b9d426f5af83
RMeuter <r.meuter@hotmail.com>
2021-11-02 22:41:26 +0100

    Permière partie des requêtes faites

--- database_Managing/database.js
+++ database_Managing/database.js
@@ -1,33 +1,92 @@
 var admin = require("firebase-admin");
-const dfd = require("danfojs-node")
+// const fetch = require('node-fetch');
+// const dfd = require("danfojs");
 
 var serviceAccount = require("./credential.json");
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-
 const db = admin.firestore();
 
 
-const data = {
-    name: 'Los Angeles',
-    state: 'CA',
-    country: 'USA'
-  };
-  
-  // Add a new document in collection "cities" with ID 'LA'
-// let setDoc = db.collection('cities').doc('LA').set(data);
 
-var csv = require('jquery-csv');
+// json_data = [{A: 1, B: 2}, {A: 1, B: 2}]
+// var df
 
-json_data = [{A: 1, B: 2}, {A: 1, B: 2}]
-//df = new dfd.DataFrame(json_data)
+// url = "https://data.mobilites-m.fr/api/points/json?types=autostop"
 
-dfd.read_csv("./database_Managing/data.csv")
+// fetch(url)
+//   .then(res => res.json())
+//   .then(json_data => {
+//     df = new dfd.DataFrame(json_data["features"]);
+//     console.log(df.head().print());
+//   });
+
+
+/*
+df1 = df.iloc({rows: [":15"], columns: [":4"]})
+console.log(df1.print())
+dfd.read_csv()
 .then(df => {
-    df1 = df.iloc({rows: [":15"], columns: [":4"]})
-    console.log(df1.print())
 }).catch(err =>{
-    console.log()
+  console.log()
 })
+*/
+
+
+// ############ ############ ############ ############ ############ ############
+// ############ ############       Get list data       ############ ############
+// ############ ############ ############ ############ ############ ############ 
+// echelle = 'Nom territoire'
+// valeur = "Grésivaudan"
+
+// const cityRef = db.collection("dataGouv_Grenoble");
+// cityRef.where(echelle, '==', valeur).get()
+// .then(querySnapshot => {
+//   li = new Array();  
+
+//   querySnapshot.docs.map(doc => { li.push(doc.data()) });
+  
+//   // res.send(li) pour le retour de l'api
+//   // =====> https://pretagteam.com/question/asyncawait-on-firebase-queries
+  
+//   console.log(li)
+// });
+
+// ############ ############ ############ ############ ############ ############
+// ############ ############       Get one data       ############ ############
+// ############ ############ ############ ############ ############ ############ 
+// id = "1861" // en str uniquement !
+
+// const cityRef = db.collection("dataGouv_Grenoble");
+// cityRef.doc(id).get()
+// .then(doc => {
+//   //   // res.send(li) pour le retour de l'api
+//   console.log(doc.data())
+// });
+
+// ############ ############ ############ ############ ############ ############
+// ############ ############       Get count data       ############ ############
+// ############ ############ ############ ############ ############ ############ 
+// echelle = 'Nom territoire'
+// valeur = "Grésivaudan"
+
+// const cityRef = db.collection("dataGouv_Grenoble");
+// cityRef.where(echelle, '==', valeur).get()
+// .then(querySnapshot => {
+//   console.log(querySnapshot.size)
+//   // res.send(querySnapshot.size) pour le retour de l'api
+//   // =====> https://pretagteam.com/question/asyncawait-on-firebase-queries
+// });
+
+// ############ ############ ############ ############ ############ ############
+// ############ ############       Post data       ############ ############
+// ############ ############ ############ ############ ############ ############ 
+// data = {
+//   "Latitude":"45.271714",
+//   "Longitude":"5.271714",
+//   "ID territoire":"28",
+// }
+
+// const cityRef = db.collection("dataGouv_Grenoble");
+// cityRef.doc().set(data);
