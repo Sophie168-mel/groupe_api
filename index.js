4f3da1b4bb2f4e29f2e4ee98100db61f10ab3e01
thierrypailhas <53034645+thierrypailhas@users.noreply.github.com>
2021-11-03 16:17:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000 ;
 const parkings = require('./parkings.json');
+//const df = require('./df.csv');
+const pouces = require('./pouce.json');
 const fs       = require('fs');
+
+const csv = require('csv-parser');
 
 // Middleware
 app.use(express.json());
@@ -13,27 +17,81 @@ app.get('/parkings', function (req, res) {
     res.status(200).json(parkings);
 });
 
+function readCSV(res,zoom,value) 
+{
+    let entries = [];
+    let resreadrow = [];
+
+    fs.createReadStream('./df.csv')
+        	.pipe(csv())
+        	.on('data', function (row) {
+					     var resreadrow = readRow(row,zoom,value);
+				             if (resreadrow != null) 
+					     { 
+						entries.push(readRow(row));
+					     }
+        		   	           })
+        	.on('end',  function () {
+            				res.send(entries);
+        			       });
+}
+
+function readRow(row,zoom,value)
+{
+	if (row[zoom] == value)
+        {
+        	return row;
+        }
+}
+
+function supSeuil(élément, indice, tableauTypé) {
+  return élément >= 10;
+}
+new Uint8Array([12, 5, 8, 130, 44]).filter(supSeuil);
+// Uint8Array [ 12, 130, 44 ]
+
+app.get('/pouces', function(req, res){
+	var zoom = req.query.zoom;
+        //var value = parseInt(req.query.value);	
+	var value = req.query.value.toString();
+
+	if ((zoom == "code_departement" | zoom == "code_commune" | zoom == "code_region")) 
+	{
+	   //readCSV(res,zoom,value);
+           const pouce = pouces.filter(function (e) {
+		   					pouce => pouce.code_departement.toString() === value);
+						    }
+           res.status(200).json(pouce); 
+
+	}
+	else 
+	{
+	  res.send("Variable zoom non definie et\/ou non valorisée dans l\'URL. Valeurs possibles: code_commune ou code_departement ou code_region");
+	}
+});
+
 //sends html, JSON or CSV depending on content negociation
 app.get('/score', function(req, res) {
-	res.format({
+        res.format({
             'application/json': function () {
-              				    res.setHeader('Content-disposition', 'attachment; filename=score.json'); //do nothing
-              				    res.set('Content-Type', 'application/json');
-              				    res.send("JSON demandé");
-	     				    },
+                                            res.setHeader('Content-disposition', 'attachment; filename=score.json'); //do nothing
+                                            res.set('Content-Type', 'application/json');
+                                            res.send("JSON demandé");
+                                            },
             'application/xml':  function () {
                                             res.setHeader('Content-disposition', 'attachment; filename=score.xml'); //do nothing
                                             res.set('Content-Type', 'application/xml');
                                             res.send("XML demandé");
+                                            },
+            'application/xml+rdf':  function () {
+                                            res.setHeader('Content-disposition', 'attachment; filename=score.xml'); //do nothing
+                                            res.set('Content-Type', 'application/xml');
+                                            res.send("XML RDF demandé");
+                                            },
+
+            'default'        :  function () {
+                                            res.status(406).send("Not acceptable");
                                             }
-	    "application/rdf+xml" : function () 
-		                            {
-					    res.setHeader('Content-disposition', 'attachment; filename=score.xml'); //do nothing
-                                            res.set('Content-Type', 'application/rdf+xml');
-                                            res.send("RDF+XML demandé");
-		    			    }
-            'default'        : function ()  {
-		    			    res.status(406).send("Not acceptable")
                                             })
 });
 
