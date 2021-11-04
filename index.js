f7fcd98768d935600e97ccbdc4a8eafd1efbab60
RMeuter <roro7@hotmail.fr>
2021-11-04 18:05:42 +0100

    add route

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ app.use(express.json());
 function IsRequestHeaderAcceptValid(req) {
 	if (req.accepts(['application/json','application/xml','application/rdf']))
         {
-                 return 1;
-        }
-	else
-	{
+        return 1;
+    } else {
                  return 0;
-        }
+    }
 }
 
 app.get('/map', function(req, res){
@@ -36,26 +34,29 @@ app.get('/map', function(req, res){
         else if(rayon < 1){
             message = "Le rayon est inférieur à 1 km"
             res.status(406).send("ERROR:"+message)
+        }
+        else {
+            database.getArround(res, collection, centerArr, rayon)
+        }
+    } else {
+        res.status(406).send("Header Accept not acceptable");
+    }
+});
 
 app.get('/pouces', function(req, res){
-	if (IsRequestHeaderAcceptValid(req)) 
-	{	
-		var zoom = req.query.zoom;
-		//var value = req.query.value.toString();
-		if ((zoom == "code_departement" | zoom == "ID territoire" | zoom == "code_region")) 
+    if (IsRequestHeaderAcceptValid(req)) {	
+		var echelle = req.query.echelle;
+		var value = req.query.value.toString();
+		if ((echelle == "code_departement" | echelle == "Nom territoire" | echelle == "code_region")) 
 		{
-     			var echelle = 'Nom territoire'
-  			var valeur = "Grésivaudan"
-     			database.GetValueWhere(res, collection, echelle, valeur)
+     		database.GetValueWhere(res, collection, echelle, value)
   		}
 		else {
 	  		res.status(406).send("Variable echelle non definie et\/ou non valorisée dans l\'URL. Valeurs possibles: 'Nom territoire'");
 		}
-	}
-	else
-	{
-       		 res.status(406).send("Header Accept not acceptable");
-        }
+	} else {
+       	res.status(406).send("Header Accept not acceptable");
+    }
 
 });
 
@@ -64,15 +65,15 @@ function IsJsonRequestBody(res,data)
 	const Ajv = require("ajv");
 	const ajv = new Ajv({allErrors: true});
         const schema = {
-                        type: "object",
-                        properties: {
-                                        "Latitude": {type: "string"},
-                                        "Longitude": {type: "string"},
-                                        "ID territoire": {type: "string"}
-                                    },
-                        required: ["Latitude","Longitude","ID territoire"],
-                        additionalProperties: false
-                        };
+            type: "object",
+            properties: {
+                "Latitude": {type: "string"},
+                "Longitude": {type: "string"},
+                "ID territoire": {type: "string"}
+                },
+            required: ["Latitude","Longitude","ID territoire"],
+            additionalProperties: false
+            };
 
         const valid = ajv.validate(schema, data);
         if (!valid) 
@@ -100,11 +101,8 @@ app.post('/pouce', function(req, res) {
         }
         else {
             database.getArround(res, collection, centerArr, rayon)
+            res.status(406).send("Header Accept not acceptable");
         }
-    } else {
-        res.status(406).send("Header Accept not acceptable");
-    }
-
 });
 
 app.listen(PORT, function () {
