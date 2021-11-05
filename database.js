51802f621be1af2ff74934cd31419689cf764fdd
RMeuter <roro7@hotmail.fr>
2021-11-05 10:30:32 +0100

    change cred

--- database.js
+++ database.js
@@ -6,16 +6,9 @@ const js2xml = require("./tool/json_to_xml")
 // Connexion à la base de donnée
 admin.initializeApp({
   credential: admin.credential.cert({
-    "type": process.env["type"],
-    "project_id": process.env["project_id"],
-    "private_key_id": process.env["private_key_id"],
-    "private_key": process.env["private_key"],
-    "client_email": process.env["client_email"],
-    "client_id": process.env["client_id"],
-    "auth_uri": process.env["auth_uri"],
-    "token_uri": process.env["token_uri"],
-    "auth_provider_x509_cert_url": process.env["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": process.env["client_x509_cert_url"] 
+    project_id: process.env.project_id,
+    client_email: process.env.client_email,
+    private_key: process.env.private_key.replace(/\\n/g, '\n'),
     })
 });
 const db = admin.firestore();
